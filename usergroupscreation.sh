977a3030e03544fb5c76b898b4d9bc08102dd4cc
faresbouzayen <118165741+faresbouzayen@users.noreply.github.com>
2024-01-13 23:45:48 +0000

    Update usergroupscreation.sh

--- usergroupscreation.sh
+++ usergroupscreation.sh
@@ -4,3 +4,13 @@ then
     echo "You must login as root to create users and groups." >&2
     exit 1
 fi
+separator=$'\n'
+read -d '' -ra credentials < $1
+separator='-'
+for one in "${credentials[@]}"
+Do
+read usr grp <<< "$one"
+useradd -m usr
+getent group $grp || groupadd $grp
+usermod -a -G $grp $usr
+pass=$(cat /dev/urandom | tr -dc A-Za-z0-9 | head -c8)

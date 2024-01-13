2bdca85ead3f69a68c955d2febe42ede3dc2beb7
faresbouzayen <118165741+faresbouzayen@users.noreply.github.com>
2024-01-13 23:01:15 +0000

    update

--- HealthCheck.sh
+++ HealthCheck.sh
@@ -19,3 +19,26 @@ echo "Configured HTTP DIRECTORY is /$HTTPDIRECTORY"
 # Prompt user for new absolute path for DocumentRoot
 read -p "Press Enter to accept the given value or enter the new absolute path for the new DocumentRoot: " ABSOLUTEPATHDIRECTORY
 ABSOLUTEPATHDIRECTORY=${ABSOLUTEPATHDIRECTORY:-$HTTPDIRECTORY}
+
+# Processing for path un-accepted characters using Sed utility
+1ABSOLUTEPATHDIRECTORY=$(echo $ABSOLUTEPATHDIRECTORY | sed -n '/^[a-zA-Z0-9\/_-]*$/p' )
+1HTTPDIRECTORY=$(echo $HTTPDIRECTORY | sed -n '/^[a-zA-Z0-9\/_-]*$/p' )
+
+# Config backup
+cp /etc/apache2/sites-available/000-default.conf /etc/apache2/l/sites-available/000-default.conf.backup
+sed -i "s/$1HTTPDIRECTORY/$1ABSOLUTEPATHDIRECTORY/g" /etc/apache2/sites-available/000-default.conf
+
+# Restart to use the new configuration
+systemctl reload apache2
+
+if [[ $? -ne 0 ]]
+then
+    cp /etc/apache2/sites-available/000-default.conf.backup /etc/apache2/sites-available/000-default.conf
+    echo "Syntax error might be your cause of issue . . . "
+    systemctl stop apache2
+    systemctl start apache2
+    return 1
+fi
+# Do not allow other channels than these
+ufw allow http
+ufw allow https
